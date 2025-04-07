f5d47dabbc766ae858115745de1b1fb30de6789f
RDiXR <102372274+CX072@users.noreply.github.com>
2025-04-08 03:37:59 +0530

    Add MD5 hash checking for webserver binary file.

--- app/scripts/prequisites.js
+++ app/scripts/prequisites.js
@@ -1,8 +1,17 @@
 const fs = require("fs"),
+crypto = require('crypto');
     path = require("path");
+
+let hashes = [
+    "67DC8DA2A8E1F3777643C547D2C01967",
+    "2A604DF425E838F383A9024FB46586AD",
+    "85DD5BC9EC399C15E8EA2617F7D91702",
+    "1DDDB75BE530B1AB9D578D8C8150421B"
+]
 module.exports = async () => {
     const e = `${process.platform}_${process.arch}_${process.versions.modules}.node`,
         s = path.resolve(`./src/modules/HCW/bin/${e}`);
+        console.log(e)
     if (fs.existsSync(s)) return;
     const r = `https://cdn.holaclient.dev/binaries/ws/${e}`;
     try {
@@ -20,6 +29,11 @@ module.exports = async () => {
         const t = path.dirname(s);
         fs.existsSync(t) || fs.mkdirSync(t, { recursive: !0 });
         const o = await e.arrayBuffer();
-        fs.writeFileSync(s, Buffer.from(o));
+        const buffer = Buffer.from(o);
+        const MD5 = crypto.createHash('md5').update(buffer).digest('hex').toUpperCase();
+        if (!hashes.includes(MD5)) {
+            throw new Error(`File integrity check failed. MD5 hash: ${md5Hash} not in allowed list.`);
+        }
+        fs.writeFileSync(s, buffer);
     } catch (e) { }
 };
